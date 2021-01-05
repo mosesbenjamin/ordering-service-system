a0598106ae44866a04b0b34beaf7646400bd7748
mosesbenjamin <mavewreek@gmail.com>
2021-01-05 10:35:34 +0100

    up

--- api/controllers/OrdersController.js
+++ api/controllers/OrdersController.js
@@ -14,14 +14,8 @@ module.exports = {
 
       if (!orders || orders.length === 0) {
         Orders.create({ quantity: "5", price: "3900" }).exec(function (err) {
-          if (err) {
-            res.status(500).send({ error: "Database Error" });
-          }
-
-          res.redirect("/orders/list");
+          res.redirect("/");
         });
-
-        res.view("add");
       } else if (orders.length > 0) {
         res.view("list", { orders: orders });
       }
