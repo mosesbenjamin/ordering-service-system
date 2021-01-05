02e4bcd0354d0cb532b03d8a6d50a365e59d7ba5
mosesbenjamin <mavewreek@gmail.com>
2021-01-05 08:50:15 +0100

    alter

--- config/env/production.js
+++ config/env/production.js
@@ -82,7 +82,7 @@ module.exports = {
      * https://sailsjs.com/docs/concepts/models-and-orm/model-settings#?migrate *
      *                                                                          *
      ***************************************************************************/
-    migrate: "safe",
+    migrate: "alter",
 
     /***************************************************************************
      *                                                                          *
